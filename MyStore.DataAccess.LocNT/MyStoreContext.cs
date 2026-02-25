9dced344dba2ed5aaf8c7b8ba99521bfe0b7d0d6
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2026-02-25 10:19:52 +0700

    update: delete comment line

--- MyStore.DataAccess.LocNT/MyStoreContext.cs
+++ MyStore.DataAccess.LocNT/MyStoreContext.cs
@@ -23,24 +23,6 @@ public partial class MyStoreContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-    //private string GetConnectionString()
-    //{
-    //    IConfiguration configuration = new ConfigurationBuilder()
-    //        .SetBasePath(Directory.GetCurrentDirectory())
-    //        .AddJsonFile("appsettings.json",true,true).Build();
-    //    return configuration["ConnectionString:DefaultConnection"];
-    //}
-
-
-    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-    //        => optionsBuilder.UseSqlServer("Server=TUANLOC;Database=MyStore;Uid=sa; Pwd=123456;TrustServerCertificate=true");
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlServer(GetConnectionString());
-    //}
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AccountMember>(entity =>
